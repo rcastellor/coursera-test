66008ddb605a15dd8c1d9737c29fe4313483bebe
rcastellor <roberto@castellor.es>
2016-09-25 21:22:37 +0200

    Added angular.min.js and final assigment for index.html and app.js

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -1,8 +1,41 @@
-(function(){
+(function () {
 'use strict';
+
+angular.module('FirstApp', [])
+
+.controller('LunchCheckController', LaunchCheckController);
+
+LaunchCheckController.$inject = ['$scope'];
     
-    angular.module('FirstApp')
-    .controler('FirstController', function($scope){
-        
-    })
-});
+function LaunchCheckController (scope) {
+  scope.items = '';
+  scope.message = '';
+  scope.clickButton = function () {
+    scope.message = getMessageString(scope.items);
+  };
+    
+    function getMessageString(items){
+        if(items == ''){
+            message = 'Please enter data first';
+        }else{
+            var itemArray = items.split(',');
+            var message = '';
+
+            var itemNumber = 0;
+
+            for(var i = 0; i<itemArray.length; i++){
+                if(itemArray[i].trim() != ''){
+                    itemNumber++;
+                }
+            }
+
+            if(itemNumber < 4){
+                message = 'Enjoy!';
+            }else{
+                message = 'Too much!';
+            }
+        }
+        return message;
+    }
+  };
+})();
